511d0f393742edfed605456a5a45fb221f431490
baimafeima123 <36907460+baimafeima123@users.noreply.github.com>
2025-04-25 15:19:56 +0000

    v0.01-2py修改bug

--- codelearn/001.py
+++ codelearn/001.py
@@ -6,37 +6,43 @@ import random
 
 options = ["+","-","*","/"]
 def get_div(anum):
+    
     l = []
-    for i in range(1,anum):
+    for i in range(1,anum+1):
         if anum%i == 0:
             l.append(i)
     return random.choice(l)
 
 
 def main():
+    question = []
     start = time.time()
     while time.time()-start<=60:
         a = random.randint(1,99)
         op = random.choice(options)
         if op == '/':
-            b = random.randint(1,99)
-        else:
             b = get_div(a)
+        else:
+            b = random.randint(1,99)
         a_op_b = str(a)+op+str(b)
         ans = int(eval(a_op_b))
 
-        try:
-            kehu = int(input(a_op_b+'='))
-        except:
-            kehu = ""
-        if kehu == ans:
-            print("CORRECT!",end=" ")
+        if 60-(time.time()-start) >= 0:
+            try:
+                kehu = int(input(a_op_b+'='))
+            except:
+                kehu = ""
+            if kehu == ans:
+                print("CORRECT!",end=" ")
+            else:
+                print("Wrong Answer",end=" ")
+            remaintime = 60-(time.time()-start)
+            print("remain time "+ str(remaintime)+ "second")
         else:
-            print("Wrong Answer",end=" ")
-        remaintime = 60-(time.time()-start)
-        print("remain time "+ str(remaintime)+ "second")
+            break
             
         
         pass
 if __name__ =='__main__':
     main()
+    print("End the game")
